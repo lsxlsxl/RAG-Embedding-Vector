091da5b3a61ab538634b1b6eb196eca3941e1613
lsxlsxl <97324314+lsxlsxl@users.noreply.github.com>
2025-05-21 15:11:29 +0800

    Update test.py

--- RAG-Embedding-Vector/test.py
+++ RAG-Embedding-Vector/test.py
@@ -3,8 +3,6 @@ from openai import OpenAI
 
 # 设置环境变量（或者确保系统环境变量中已有）
 os.environ["OPENAI_API_KEY"] = "your api key"
-# 如果有自定义 Base URL
-# os.environ["OPENAI_API_BASE"] = "https://api.openai-hk.com/v1"
 
 client = OpenAI()
 
